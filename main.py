2e43e6bab389570da9efab81edff28932a741016
unknown <n94583181@gmail.com>
2026-06-09 23:26:18 +0600

    main is changed

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import uvicorn
 from travel_app.admin.setup import setup_admin
 from travel_app.api import users, categories, places, tours, bookings, reviews, gallery, auth
 
-tour_app = FastAPI(title="Tour_app")
+travel_app = FastAPI(title="Tour_app")
 
-tour_app.include_router(auth.auth_router)
-tour_app.include_router(users.user_router)
-tour_app.include_router(categories.category_router)
-tour_app.include_router(places.place_router)
-tour_app.include_router(tours.tour_router)
-tour_app.include_router(bookings.booking_router)
-tour_app.include_router(reviews.review_router)
-tour_app.include_router(gallery.gallery_router)
+travel_app.include_router(auth.auth_router)
+travel_app.include_router(users.user_router)
+travel_app.include_router(categories.category_router)
+travel_app.include_router(places.place_router)
+travel_app.include_router(tours.tour_router)
+travel_app.include_router(bookings.booking_router)
+travel_app.include_router(reviews.review_router)
+travel_app.include_router(gallery.gallery_router)
 
-setup_admin(tour_app)
+setup_admin(travel_app)
 
 if __name__ == '__main__':
-    uvicorn.run(tour_app, host="127.0.0.1", port=8000)
+    uvicorn.run(travel_app, host="127.0.0.1", port=8000)
